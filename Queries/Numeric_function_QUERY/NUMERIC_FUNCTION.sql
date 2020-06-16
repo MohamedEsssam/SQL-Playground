fd6f2dd81af0b9af946e3aba38443f0b7ea30dc4
MohamedEsssam <me547010@gmail.com>
2020-06-16 02:05:48 +0200

    NUMERIC FUNCTION QUERY

---
+++ Queries/Numeric_function_QUERY/NUMERIC_FUNCTION.sql
@@ -0,0 +1,31 @@
+/**
+  ROUND/TRUNCATE
+  second argument for
+  procedure for rounding
+ */
+SELECT ROUND(5.7342423, 2);
+SELECT ROUND(5.2);
+SELECT TRUNCATE(5.7342423, 2);
+
+/**
+  CEILING will return the
+  smallest int that greater than this number
+ */
+SELECT CEILING(5.2);
+
+/**
+  CEILING will return the
+  smallest int that less than this number
+ */
+SELECT FLOOR(5.2);
+
+/**
+  ABS return absolute value
+ */
+SELECT ABS(-2);
+
+/**
+  RAND generate random number
+  between 0 and 1
+ */
+SELECT RAND()
